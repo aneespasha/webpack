c826fb8434ebbc05b928472566f64c13d25b0a2d
aneespasha <aneespasha@gmail.com>
2018-03-26 18:00:12 +0530

    webpack improvment

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
-import 'bootstrap';
-import 'jquery';
-import 'jquery-datepicker';
-import '../src/styles/appStyles.scss';
+function component() {
+    var element = document.createElement('div');
+
+    // Lodash, currently included via a script, is required for this line to work
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    return element;
+}
+
+document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,9 @@
 const path = require('path');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-
 
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'assets')
-    },
-    module:{
-        rules:[
-            {
-                test:/\.(s*)css$/,
-                use: ExtractTextPlugin.extract({
-                    fallback:'style-loader',
-                    use:['css-loader','sass-loader'],
-                })
-            }
-        ]
-    },
-    plugins: [
-        new ExtractTextPlugin({filename:'app.bundle.css'})
-    ]
+        path: path.resolve(__dirname, 'dist')
+    }
 };
