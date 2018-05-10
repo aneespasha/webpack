095cf533763c5a91bb08bf198bd229767bda7349
aneespasha <aneespasha@gmail.com>
2018-05-10 18:16:17 +0530

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -3,22 +3,32 @@
 const path = require('path');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const styleLintPlugin = require('stylelint-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 require('es6-promise').polyfill();
 
 module.exports = {
-  entry: './app.js',
+
+  entry: './src/app.js',
 
   output: {
     filename: 'js/app.js',
-    path: path.resolve(__dirname, '')
+    path: path.resolve(__dirname, 'dist')
   },
 
   plugins: [
+
     // Specify the resulting CSS filename
     new ExtractTextPlugin({ // define where to save the file
       filename: 'css/app.min.css',
       allChunks: true
+    }),
+
+    new HtmlWebpackPlugin({
+      hash: true,
+      template: './src/index.html',
+      filename: './index.html' //relative to root of the application
     }),
 
     // Stylelint plugin
@@ -34,7 +44,21 @@ module.exports = {
   ],
 
   module: {
+
     rules: [
+
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,    //to support @font-face rule
+        loader: "url-loader",
+        query: {
+          limit: '10000',
+          name: '[name].[ext]',
+          outputPath: 'fonts/'
+          //the fonts will be emmited to public/assets/fonts/ folder
+          //the fonts will be put in the DOM <style> tag as eg. @font-face{ src:url(assets/fonts/font.ttf); }
+        }
+      },
+      
       {
         test: /\.js$/,
         exclude: /node_modules/,
@@ -42,6 +66,7 @@ module.exports = {
           'babel-loader'
         ]
       },
+
       {
         test: /\.(css|sass|scss)$/,
         use: ExtractTextPlugin.extract({
@@ -54,15 +79,45 @@ module.exports = {
           publicPath: "../"
         })
       },
+
       {
-        test: /\.(png|jp(e*)g|gif)$/,
+        test: /\.(png|jp(e*)g|svg|gif)$/,
         use: [{
           loader: 'url-loader',
           options: {
-            limit: 8000, // Convert images < 8kb to base64 strings [hash]-[name].[ext]
-            name: 'images/[hash]-[name].[ext]'
+            limit: 80000, // Convert images < 8kb to base64 strings
+            name: '[hash]-[name].[ext]',
+            outputPath: 'images'
           }
         }]
+      }
+    ],
+
+    loaders: [
+      {test: /\.(png|jpg|jpeg|gif|svg)$/, loader: "url-loader?limit=100000"},
+
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+      },
+
+      {
+        test: /\.(ttf|otf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?|(jpg|gif)$/,
+        loader: 'file-loader'
+      },
+
+      {
+        test: /\.html$/,
+        loader: "file-loader?name=[name].[ext]",
+      },
+
+      {
+        loader: 'postcss-loader',
+        options: {
+          config: {
+            path: 'postcss.config.js',
+          }
+        }
       }
     ]
   },
